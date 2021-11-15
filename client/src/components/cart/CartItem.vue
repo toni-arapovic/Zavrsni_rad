e9de411b75347f66c3402d8aa67f182cd38955d4
Toni Arapovic <toni.arapovic@outlook.com>
2021-11-15 23:10:36 +0100

    Added product removal confirmation

--- client/src/components/cart/CartItem.vue
+++ client/src/components/cart/CartItem.vue
@@ -9,11 +9,29 @@
         <p class="card-text">Kolicina {{ qty }}</p>
       </div>
       <div class="buy d-flex justify-content-between align-items-center">
-      <h5  class="price text-success">Cijena {{ price }}KM</h5>
-      <button @click="remove" class="btn btn-danger">Ukloni</button>
+        <h5  class="price text-success">Cijena {{ price }}KM</h5>
+        <button  class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Ukloni</button>
+      </div>
+    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ukloni proizvod</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Jeste li sigurni da želite ukloniti proizvod?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Odustani</button>
+            <button @click="remove" type="button" class="btn btn-danger" data-bs-dismiss="modal">Ukloni</button>
+          </div>
+        </div>
       </div>
     </div>
   </li>
+  
 </template>
 
 
